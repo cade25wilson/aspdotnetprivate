9c3da26a2586cfdedb40422fbf3467d094184ffb
Cade Wilson <cade25wilson@gmail.com>
2021-12-01 12:27:54 -0700

    delete controller
    updated delete controller to not delete blankImage in wwwroot

--- WebApplication3/WebApplication3/Controllers/ProductsController.cs
+++ WebApplication3/WebApplication3/Controllers/ProductsController.cs
@@ -335,7 +335,11 @@ namespace WebApplication3.Controllers
             string[] imagePaths = { imagePath, imagePath2, imagePath3, imagePath4, imagePath5, imagePath6, imagePath7, imagePath8, imagePath9, imagePath10 };
             foreach (var p in imagePaths)
             {
-                if (System.IO.File.Exists(p))
+                if (p == Path.Join(_hostEnvironment.WebRootPath, "image", "blankImage.jpg"))
+                {
+                    Console.WriteLine("do nothing");            
+                }
+                else if (System.IO.File.Exists(p))
                 {
                     System.IO.File.Delete(p);
                 }
